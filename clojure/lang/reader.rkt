487f6ca9c87a7676f5ce97f92b8ab0228ae668e6
AlexKnauth <alexander@knauth.org>
2015-06-07 21:25:21 -0400

    make `~` unquote and `,` whitespace

--- clojure/lang/reader.rkt
+++ clojure/lang/reader.rkt
@@ -1,2 +1,11 @@
 #lang s-exp syntax/module-reader
 clojure/clojure
+#:wrapper1
+(lambda (t)
+  (parameterize ([current-readtable (make-clojure-readtable)])
+    (t)))
+(define (make-clojure-readtable [rt (current-readtable)])
+  (make-readtable rt
+                  #\~ #\, #f
+                  #\, #\space #f
+                  ))
